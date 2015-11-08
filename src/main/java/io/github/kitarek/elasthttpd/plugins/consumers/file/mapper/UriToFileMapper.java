c454e12be303f3e88763726f87dd799cf78645c3
Arkadiusz /kitarek/ Kita <kitarek@gmail.com>
2015-11-09 00:45:25 +0100

    Implemented UriToFileMapper with unit tests.

--- src/main/java/io/github/kitarek/elasthttpd/plugins/consumers/file/mapper/UriToFileMapper.java
+++ src/main/java/io/github/kitarek/elasthttpd/plugins/consumers/file/mapper/UriToFileMapper.java
@@ -17,5 +17,52 @@
 
 package io.github.kitarek.elasthttpd.plugins.consumers.file.mapper;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.apache.commons.lang3.StringUtils.*;
+import static org.apache.commons.lang3.Validate.*;
+
+
 public class UriToFileMapper {
+
+	public static final String ROOT_URI_REQUEST_PATH = "/";
+	public static final String RESOURCE_PATH_SEPARATOR = "/";
+	private final String pathToMappedRootDirectory;
+
+	public UriToFileMapper(String pathToMappedRootDirectory) {
+		this.pathToMappedRootDirectory =
+				defaultIfEmpty(
+					removeEnd(
+						notEmpty(pathToMappedRootDirectory, "Path of root directory for mapping cannot be empty"),
+					RESOURCE_PATH_SEPARATOR),
+				ROOT_URI_REQUEST_PATH);
+		isTrue(isValidDirectoryForMapping(this.pathToMappedRootDirectory),
+				"Path needs to point to needs to be an absolute existing and readable directory that will be mapped.");
+	}
+
+	public static boolean isValidDirectoryForMapping(String absolutePathToDirectory) {
+		notNull(absolutePathToDirectory, "An absolute path to directory cannot be null");
+		return isValidExistingAbsoluteDirectory(new File(absolutePathToDirectory));
+	}
+
+	private static boolean isValidExistingAbsoluteDirectory(File f) {
+		return f.canRead() && f.exists() && f.isAbsolute() && f.isDirectory();
+	}
+
+	public static boolean isCorrectUriRequestPath(String uriRequestPath) {
+		return !isBlank(uriRequestPath) && uriRequestPath.startsWith(ROOT_URI_REQUEST_PATH);
+	}
+
+	public String mapUriRequestPath(String uriRequestPath) {
+		isTrue(isCorrectUriRequestPath(uriRequestPath), "URI request path is not correct");
+		return removeEnd(pathToMappedRootDirectory + normalizeUrlRequestPath(uriRequestPath), RESOURCE_PATH_SEPARATOR);
+	}
+
+	private String normalizeUrlRequestPath(String uriRequestPath) {
+		Path p = Paths.get(uriRequestPath);
+		return p.normalize().toAbsolutePath().toString();
+	}
+
 }
