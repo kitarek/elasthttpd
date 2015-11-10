16bd70a790f919d44f12dc823c8b98cee9284431
Arkadiusz /kitarek/ Kita <kitarek@gmail.com>
2015-11-11 00:42:17 +0100

    Almost fully implemented HttpFileProducer. A few unhappy branches not covered yet.

--- src/main/java/io/github/kitarek/elasthttpd/plugins/consumers/file/producer/HttpFileProducer.java
+++ src/main/java/io/github/kitarek/elasthttpd/plugins/consumers/file/producer/HttpFileProducer.java
@@ -17,22 +17,157 @@
 
 package io.github.kitarek.elasthttpd.plugins.consumers.file.producer;
 
+import io.github.kitarek.elasthttpd.commons.Optional;
+import io.github.kitarek.elasthttpd.commons.OptionalDispatcher;
 import org.apache.http.HttpResponse;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.InputStreamEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.github.kitarek.elasthttpd.commons.Optional.empty;
+import static io.github.kitarek.elasthttpd.commons.Optional.present;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.Validate.isTrue;
+import static org.apache.commons.lang3.Validate.notNull;
+import static org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR;
+import static org.apache.http.util.EncodingUtils.getAsciiBytes;
 
 /**
- * Allows to construct HTTP response that will send over HTTP protocol an existing file from local filesystem
+ * Allows to setup HTTP response that will send over HTTP protocol an existing file from local filesystem using
+ * the HTTP entity encoding
+ *
+ * Please note that this implementation currently does not handle charset detection of a file
  */
 public class HttpFileProducer {
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(HttpFileProducer.class);
+	public static final int STREAM_BUFFER_100KB_SIZE = 102400;
+
 	/**
-	 * Transform HttpResponse in such way it will contain the specified fle
+	 * Transform HttpResponse in such way it will contain the specified file as HTTP entity.
 	 *
 	 * @param localFile a valid not null file instance that must exist and be readable
 	 * @param response a valid initially preconfigured HTTP response
 	 */
-	public void sendFileOverHttpResponse(File localFile, HttpResponse response) {
+	public void sendFileOverHttpResponse(final File localFile, final HttpResponse response) {
+		notNull(localFile, "Local file must be not null");
+		notNull(response, "HTTP response must be not null");
+		isTrue(localFile.exists(), "The local file needs to exists: %s", localFile);
+		isTrue(localFile.isFile(), "THe local file needs to be file in real and neither a directory nor device: %s",
+				localFile);
+		isTrue(!localFile.isDirectory(), "The local file must not be a directory: %s", localFile);
+		isTrue(localFile.canRead(), "The local file must be at least readable: %s", localFile);
 
+
+		final Optional<String> optionalContentType = detectMimeContentType(localFile);
+		final Optional<InputStream> optionalInputStreamFromFile = transformFileIntoInputStream(localFile);
+		final FileEntityMetadata fileEntityMetadata = new FileEntityMetadata(optionalContentType, localFile.length());
+		sendInputStreamOverHttpResponse(optionalInputStreamFromFile, response, fileEntityMetadata);
+	}
+
+	private void sendInputStreamOverHttpResponse(Optional<InputStream> optionalInputStreamFromFile,
+												 final HttpResponse response,
+												 final FileEntityMetadata fileEntityMetadata) {
+		optionalInputStreamFromFile.dispatch(new OptionalDispatcher<InputStream>() {
+			public void notPresent() {
+				setupInternalServerErrorResponse(response);
+			}
+
+			public void present(InputStream inputStream) {
+				final FileEntity fileEntity = new FileEntity(inputStream, fileEntityMetadata);
+				transformFileEntityIntoHttpEntity(fileEntity, response);
+			}
+		});
+	}
+
+	private Optional<InputStream> transformFileIntoInputStream(File localFile) {
+		Optional<InputStream> optionalInputStreamFromFile;
+		try {
+			final InputStream inputStream = new BufferedInputStream(new FileInputStream(localFile),
+					STREAM_BUFFER_100KB_SIZE);
+			optionalInputStreamFromFile = present(inputStream);
+		} catch (FileNotFoundException e) {
+			optionalInputStreamFromFile = empty();
+			LOGGER.error(format("Internal Server Error: Cannot open and read file: %s", localFile.getAbsolutePath()), e);
+		}
+		return optionalInputStreamFromFile;
+	}
+
+	private void transformFileEntityIntoHttpEntity(final FileEntity fileEntity, final HttpResponse response) {
+		final FileEntityMetadata fileEntityMetadata = fileEntity.getFileEntityMetadata();
+		fileEntityMetadata.getOptionalContentType().dispatch(new OptionalDispatcher<String>() {
+			public void notPresent() {
+				response.setEntity(new InputStreamEntity(fileEntity.getInputStreamFromFile(),
+						fileEntityMetadata.getFileLength()));
+			}
+
+			public void present(String contentType) {
+				response.setEntity(new InputStreamEntity(fileEntity.getInputStreamFromFile(),
+						fileEntityMetadata.getFileLength(), ContentType.create(contentType)));
+			}
+		});
+	}
+
+	private Optional<String> detectMimeContentType(File localFile) {
+		Optional<String> optionalContentType;
+		try {
+			String contentType = Files.probeContentType(Paths.get(localFile.getAbsolutePath()));
+			optionalContentType = present(contentType);
+		} catch (IOException e) {
+			optionalContentType = empty();
+			LOGGER.warn("There was an error checking file content type", e);
+		}
+		return optionalContentType;
+	}
+
+	private void setupInternalServerErrorResponse(HttpResponse response) {
+		response.setStatusCode(SC_INTERNAL_SERVER_ERROR);
+		response.setReasonPhrase("INTERNAL SERVER ERROR");
+		response.setEntity(new ByteArrayEntity(getAsciiBytes(
+				"Internal Server Error while opening/reading server resource"),
+				ContentType.create("text/plain", "US-ASCII")));
+	}
+
+
+	private static class FileEntity {
+		private final InputStream inputStreamFromFile;
+		private final FileEntityMetadata fileEntityMetadata;
+
+		private FileEntity(InputStream inputStreamFromFile, final FileEntityMetadata fileEntityMetadata) {
+			this.fileEntityMetadata = notNull(fileEntityMetadata);
+			this.inputStreamFromFile = notNull(inputStreamFromFile);
+		}
+
+		public FileEntityMetadata getFileEntityMetadata() {
+			return fileEntityMetadata;
+		}
+
+		public InputStream getInputStreamFromFile() {
+			return inputStreamFromFile;
+		}
+	}
+
+	private static class FileEntityMetadata {
+		private final Optional<String> optionalContentType;
+		private final long fileLength;
+
+		private FileEntityMetadata(Optional<String> optionalContentType, long fileLength) {
+			this.optionalContentType = notNull(optionalContentType);
+			this.fileLength = notNull(fileLength);
+		}
+
+		public Optional<String> getOptionalContentType() {
+			return optionalContentType;
+		}
+
+		public long getFileLength() {
+			return fileLength;
+		}
 	}
 }
--- src/test/groovy/io/github/kitarek/elasthttpd/plugins/consumers/file/producer/HttpFileProducerSpec.groovy
+++ src/test/groovy/io/github/kitarek/elasthttpd/plugins/consumers/file/producer/HttpFileProducerSpec.groovy
@@ -17,7 +17,134 @@
 
 package io.github.kitarek.elasthttpd.plugins.consumers.file.producer
 
+import org.apache.http.HttpResponse
+import org.apache.http.entity.AbstractHttpEntity
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class HttpFileProducerSpec extends Specification {
+
+	def 'Always can create instance without giving any parameters'() {
+		when:
+			new HttpFileProducer()
+		then:
+			notThrown()
+	}
+
+	@Unroll
+	def 'Never cannot pass null local file or null HTTP response for sending via HTTP protocol'() {
+		given:
+			def HttpFileProducer producer = new HttpFileProducer();
+
+		when:
+			producer.sendFileOverHttpResponse(localFile, httpResponse)
+
+		then:
+			thrown(NullPointerException)
+
+		where:
+			localFile  | httpResponse
+			null       | Mock(HttpResponse)
+			Mock(File) | null
+			null       | null
+	}
+
+	def 'Never cannot pass local file that does not exist'() {
+		given:
+			def HttpFileProducer producer = new HttpFileProducer();
+			def File localFile = Stub(File)
+			localFile.exists() >> false
+
+		when:
+			producer.sendFileOverHttpResponse(localFile, Mock(HttpResponse))
+
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	def 'Never cannot pass local file that is not a file (i.e. directory instead)'() {
+		given:
+			def HttpFileProducer producer = new HttpFileProducer();
+			def File localFile = Stub(File)
+			localFile.exists() >> true
+			localFile.isFile() >> false
+
+		when:
+			producer.sendFileOverHttpResponse(localFile, Mock(HttpResponse))
+
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	def 'Never cannot pass local file that is not readable'() {
+		given:
+			def HttpFileProducer producer = new HttpFileProducer();
+			def File localFile = Stub(File)
+			localFile.exists() >> true
+			localFile.isFile() >> true
+			localFile.canRead() >> false
+
+		when:
+			producer.sendFileOverHttpResponse(localFile, Mock(HttpResponse))
+
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	def 'Never cannot pass local file that is a directory'() {
+		given:
+			def HttpFileProducer producer = new HttpFileProducer();
+			def File localFile = Stub(File)
+			localFile.exists() >> true
+			localFile.isFile() >> false
+			localFile.isDirectory() >> true
+			localFile.canRead() >> true
+
+		when:
+			producer.sendFileOverHttpResponse(localFile, Mock(HttpResponse))
+
+		then:
+			thrown(IllegalArgumentException)
+	}
+
+	def 'Always can pass real local file that is a readable and existing one'() {
+		given:
+			def HttpFileProducer producer = new HttpFileProducer();
+			def projectTestFilePath = "/src/test/resources/test-file.txt"
+			def mimeType = 'text/plain'
+			def File localRealTextFile = new File(currentExistingProjectDirectory() + projectTestFilePath)
+			def filesize = localRealTextFile.size()
+		and:
+			def httpResponse = Mock(HttpResponse)
+			def catchedEntity
+
+		when:
+			producer.sendFileOverHttpResponse(localRealTextFile, httpResponse)
+
+		then:
+			1 * httpResponse.setEntity(_) >> { args ->
+				catchedEntity = args[0]
+			}
+		and:
+			catchedEntity != null
+			catchedEntity instanceof AbstractHttpEntity
+
+		when:
+			def AbstractHttpEntity catchedHttpEntity = catchedEntity
+
+		then:
+			catchedHttpEntity.contentType.value == mimeType
+			catchedHttpEntity.contentLength == filesize
+//			catchedHttpEntity.contentEncoding.value == null
+	}
+
+	@Shared
+	private currentExistingProjectDirectory = {
+		Path currentRelativePath = Paths.get("");
+		currentRelativePath.toAbsolutePath().toString();
+	}
 }
