59e16eee84887fdc05112f0ca4220ec24ead474a
Arkadiusz /kitarek/ Kita <kitarek@gmail.com>
2015-11-13 01:07:40 +0100

    Added logging for DefaultHttpServer

--- src/main/java/io/github/kitarek/elasthttpd/DefaultHttpServer.java
+++ src/main/java/io/github/kitarek/elasthttpd/DefaultHttpServer.java
@@ -17,8 +17,17 @@
 
 package io.github.kitarek.elasthttpd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.github.kitarek.elasthttpd.server.networking.NetworkConfigurationBuilder.DEFAULT_IPV4_LOOPBACK_LISTEN_ADDRESS;
+import static io.github.kitarek.elasthttpd.server.networking.NetworkConfigurationBuilder.DEFAULT_LISTEN_PORT;
+
 public class DefaultHttpServer {
+	public static final Logger LOGGER = LoggerFactory.getLogger(DefaultHttpServer.class);
 	public static void main(String[] args) {
+		LOGGER.info(String.format("Starting dummy webserver listening on http://%s:%s",
+				DEFAULT_IPV4_LOOPBACK_LISTEN_ADDRESS.getHostName(), DEFAULT_LISTEN_PORT));
 		ElastHttpD.startBuilding().run();
 	}
 }
